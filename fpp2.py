b22486bd870b9527c4829ccf8116b7325128c284
StarFc <79867299+StarFc@users.noreply.github.com>
2021-03-03 10:12:16 -0800

    Add files via upload

---
+++ fpp2.py
@@ -0,0 +1,37 @@
+"""
+March 2, 2021
+
+Use this document to record your answers to the Parson's Problems
+shared in class.
+
+Remember: the idea behind a Parson's Problem is that you are
+shown completely correct code that is presented OUT OF ORDER.
+When arranged correctly, the code does what the Parson's Problem
+says it should do.
+
+"""
+
+for x in range(0,10):
+    print("hello world")
+    #basic for loop
+
+#basic list
+     myList=[]print(myList)
+        myList.appened("apples")
+        myList.appened("pineapples")
+        print(myList)
+       
+    
+
+#basic Conditional
+    x= int(x)if x > 0:
+    x= input(" Type a number")else:
+    if x > 0: 
+    print("That're more than nothing!")
+    else:
+    print("That's a small number!")
+           
+     
+                
+            
+
